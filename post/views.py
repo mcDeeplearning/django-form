de553aa9d774c46b6bffd65e3dfedb06b9f049e1
changhee <change@hphk.kr>
2018-11-13 05:24:40 +0000

    crud 완성

--- post/views.py
+++ post/views.py
@@ -25,4 +25,27 @@ def create(request):
 def detail(request,id):
     post = Post.objects.get(id=id)
     return render(request,'post/detail.html',{'post':post})
+
+def update(request,id):
+    post = Post.objects.get(id=id)
+    if request.method == "POST":
+        # 폼을 검증 후 수정 저장 하는 단계
+        form = PostForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data.get('title')
+            content = form.cleaned_data.get('content')
+            # post.update(title=title,content=content)
+            post.title = title
+            post.content = content
+            post.save()
+            return redirect(resolve_url('post:detail',id))
+    else:
+        # 기존의 데이터를 폼에 담아서 사용자에게 전달
+        form = PostForm({'title':post.title,'content':post.content})
+    return render(request, 'post/update.html',{'form':form,'post':post})
+
+def delete(request,id):
+    post = Post.objects.get(id=id)
+    post.delete()
     
+    return redirect(resolve_url('post:list'))
