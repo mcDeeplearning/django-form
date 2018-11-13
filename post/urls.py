cefab0f4e3df11e0c16b3248426970c1a6d72fdb
changhee <change@hphk.kr>
2018-11-13 02:08:57 +0000

    create로직 완성

--- post/urls.py
+++ post/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+# post:create
+app_name = 'post'
+
 urlpatterns = [
-    path('',views.list),
+    path('',views.list, name='list'),
+    path('create/',views.create, name='create'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, resolve_url
+from .forms import PostForm
+from .models import Post
 
 # Create your views here.
 def list(request):
     return render(request,'post/list.html')
+    
+def create(request):
+    if request.method == "POST":
+        # 저장로직
+        form = PostForm(request.POST)
+        if form.is_valid():
+            # title = form.cleand_data['title']
+            title = form.cleaned_data.get('title')
+            content = form.cleaned_data.get('content')
+            Post.objects.create(title=title,content=content)
+            return redirect(resolve_url('post:list'))
+    else:
+        # 입력할 수 있는 폼 리턴
+        form = PostForm()
+    return render(request, 'post/create.html',{'form':form})
+        
