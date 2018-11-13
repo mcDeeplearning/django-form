577cb7efdbcd378730a5097dda1b182a45c73788
changhee <change@hphk.kr>
2018-11-13 00:51:19 +0000

    어드민 설정 완료

--- post/models.py
+++ post/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = model.CharField(max_length=50)
-    content = model.CharField(max_length=100)
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=100)
     
     def __str__(self):
         return self.title
